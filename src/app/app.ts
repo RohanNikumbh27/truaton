1eaa450a54138e320d6d0388bb09e7fde2b6644a
Rohan Kailas Nikumbh <rohannikumbh@Rohans-MacBook-Air.local>
2025-07-13 17:37:31 +0530

    Initial real ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Services } from './components/services/services';
+import { Internships } from './components/internships/internships';
+import { Community } from './components/community/community';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Contact, Footer, Hero, Services, Internships, Community],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
