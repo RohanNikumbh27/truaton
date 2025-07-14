b141d5561f1117c0de8855d68273cb9c500a7bf0
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2025-07-14 23:04:34 +0530

    internships

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Application } from './components/reusable/application/application';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'apply', component: Application },
+];
--- src/app/components/internships/internships.ts
+++ src/app/components/internships/internships.ts
@@ -1,11 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-internships',
-  imports: [],
+  imports: [ ],
   templateUrl: './internships.html',
   styleUrl: './internships.css'
 })
 export class Internships {
+  internshipsArr = [
+    {
+      label: "Frontend Developer",
+      link: "/frontend-developer-internship"
+    },
+    {
+      label: "Backend Developer",
+      link: "/backend-developer-internship"
+    },
+    {
+      label: "UI/UX Designer",
+      link: "/ui-ux-internship"
+    },
+    {
+      label: "Software Developer",
+      link: "/software-developer-internship"
+    },
+    {
+      label: "Junior HR",
+      link: "/junior-human-resource-internship"
+    },
+    {
+      label: "Business Analyst",
+      link: "/business-analyst-internship"
+    },
+    {
+      label: "Research & Development",
+      link: "/rnd-internship"
+    },
+    {
+      label: "QA",
+      link: "/qa-internship"
+    },
+    {
+      label: "Software Tester",
+      link: "/software-tester-internship"
+    },
+    {
+      label: "Business Executive",
+      link: "/business-executive-internship"
+    },
+  ]
 
 }
